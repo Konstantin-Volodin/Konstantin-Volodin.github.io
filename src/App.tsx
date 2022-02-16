e769ad3dc9dd6f29675ae2135a775a61a1b7b541
unknown <volodin.kostia@gmail.com>
2022-02-16 17:32:46 -0500

    Revert "Updates"
    This reverts commit e3abc6f4fcb5f86ccc4a6f53d80fef010583f2fb.

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import {
+  ChakraProvider,
+  extendTheme
+} from '@chakra-ui/react';
+
+import 'typeface-roboto'
+
+import Header from './components/header';
+import Intro from './components/intro';
+
+const theme = extendTheme({
+  fonts: 'Roboto'
+})
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Header></Header>
+      <Intro></Intro>
+    </ChakraProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Projects.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+import {
+  Box,
+  Flex,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalBody,
+  ModalCloseButton,
+  VStack,
+  Container
+} from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons';
