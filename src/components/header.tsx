7308f52859c57d8012ce5b7c3d5e36c6c69713a3
Konstantin-Volodin <63243086+Konstantin-Volodin@users.noreply.github.com>
2023-02-09 21:00:27 -0500

    header update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 import {
   Box, Flex, Container, Text, Heading, Link, LinkOverlay, LinkBox,
   HStack, IconButton, Spacer, Divider,
-  useDisclosure,  Modal, ModalOverlay, ModalContent, ModalBody, ModalCloseButton,
+  useDisclosure, Modal, ModalOverlay, ModalContent, ModalBody, ModalCloseButton,
 } from '@chakra-ui/react';
 import { HamburgerIcon } from '@chakra-ui/icons';
 import Resume from "../assets/resume.pdf";
@@ -9,15 +9,15 @@ import "../index.css";
 
 const Links = [
   // {'name': 'about', 'link': '#Projects', 'type': 'internal'},
-  {'name': 'github', 'link': 'https://github.com/Konstantin-Volodin', 'type': 'external'},
-  {'name': 'linkedin', 'link': 'https://www.linkedin.com/in/konstantin-volodin-90b04623b/', 'type': 'external'},
-  {'name': 'resume', 'link': Resume, 'type': 'external'},
+  { 'name': 'github', 'link': 'https://github.com/Konstantin-Volodin', 'type': 'external' },
+  { 'name': 'linkedin', 'link': 'https://www.linkedin.com/in/konstantin-volodin-90b04623b/', 'type': 'external' },
+  { 'name': 'resume', 'link': Resume, 'type': 'external' },
 ];
 
-function NavLink(props : any) {
+function NavLink(props: any) {
   console.log(props.type)
-  return(
-    <Link px={6} py={3} my={4} href={props.link} _hover={{textDecoration: 'none', bg: 'gray.200',}} isExternal={props.type == 'internal' ? false : true}>
+  return (
+    <Link px={6} py={3} my={4} href={props.link} _hover={{ textDecoration: 'none', bg: 'gray.200', }} isExternal={props.type == 'internal' ? false : true}>
       <Text letterSpacing='0.5px' textTransform='uppercase' fontWeight='600'>
         {props.name}
       </Text>
@@ -42,44 +42,48 @@ function Header() {
             {/* Large Screen Links */}
             <LinkBox>
               <HStack spacing={6} display={{ base: 'none', md: 'flex' }}>
-                {Links.map((link) => {return(
-                  <NavLink key={link.name} name={link.name} link={link.link} type={link.type} />
-                )})}
+                {Links.map((link) => {
+                  return (
+                    <NavLink key={link.name} name={link.name} link={link.link} type={link.type} />
+                  )
+                })}
                 <Box px={4} py={2} >
-                  <LinkOverlay  href="mailto:volodin.kostia@gmail.com" isExternal>
-                    <button className="btn btn-1 btn-1d"> 
+                  <LinkOverlay href="mailto:volodin.kostia@gmail.com" isExternal>
+                    <button className="btn btn-1 btn-1d">
                       email me
                     </button>
                   </LinkOverlay >
                 </Box>
               </HStack>
             </LinkBox>
-            
+
             {/* Small Screen Button */}
-            <IconButton size={'md'} display={{ base: 'flex', md: 'none' }} 
-              aria-label={'Open Menu'} icon={<HamburgerIcon/>} 
+            <IconButton size={'md'} display={{ base: 'flex', md: 'none' }}
+              aria-label={'Open Menu'} icon={<HamburgerIcon />}
               onClick={isOpen ? onClose : onOpen}
             />
           </Flex>
         </Container>
-        
+
         {/* Small Screen Links */}
-          <Modal isOpen={isOpen} onClose={onClose} isCentered size='full'>
-            <ModalOverlay />
-            <ModalContent opacity={"0.9 !important"} borderRadius='none'>
-              <ModalCloseButton />
+        <Modal isOpen={isOpen} onClose={onClose} isCentered size='full'>
+          <ModalOverlay />
+          <ModalContent opacity={"0.9 !important"} borderRadius='none'>
 
-              <ModalBody>
-                  <Flex direction='column' justify='center' align='center' opacity={"1 !important"} h="95vh">
+            <ModalBody p={0} m={0}>
+              <Box h={16} p={0} m={0}><ModalCloseButton top="12px" right='16px' w="40px" h="40px" /></Box>
+              <Flex direction='column' justify='center' align='center' opacity={"1 !important"} h='calc(100vh - 128px)'>
 
-                    {Links.map((link) => {return(
-                      <NavLink key={link.name} name={link.name} link={link.link} />
-                    )})}
-                    <NavLink name='email me' link="mailto:volodin.kostia@gmail.com"></NavLink>
-                  </Flex>
-              </ModalBody>
-            </ModalContent>
-          </Modal>
+                {Links.map((link) => {
+                  return (
+                    <NavLink key={link.name} name={link.name} link={link.link} />
+                  )
+                })}
+                <NavLink name='email me' link="mailto:volodin.kostia@gmail.com"></NavLink>
+              </Flex>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
 
         <Divider />
       </Box>
