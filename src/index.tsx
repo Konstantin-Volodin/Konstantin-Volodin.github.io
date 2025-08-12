7ffc2e7f5dd3a2d2745c1e76d190cbd23a5c3113
Konstantin-Volodin <63243086+Konstantin-Volodin@users.noreply.github.com>
2025-08-11 20:16:25 -0400

    merged into master

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,7 @@
-import "react-app-polyfill/ie11";
-import "react-app-polyfill/stable";
-import 'core-js/stable'
-
-// Import Inter font with font-display: swap for optimal CLS
-import '@fontsource/inter'; // Includes 400 and 700 weights with font-display: swap
+import '@fontsource/inter';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { ColorModeScript } from '@chakra-ui/react';
 import reportWebVitals from './reportWebVitals';
 import theme from './static/fonts/theme';
@@ -14,15 +9,15 @@ import theme from './static/fonts/theme';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootEl = document.getElementById('root');
+if (rootEl) {
+  const root = createRoot(rootEl);
+  root.render(
+    <React.StrictMode>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </React.StrictMode>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
